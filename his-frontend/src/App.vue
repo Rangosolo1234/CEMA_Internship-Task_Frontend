c681fbbafe7a71560b0d7cb9bec70b2b7eb9dc0d
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-04-28 20:34:29 +0300

    Main app file

---
+++ his-frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-blue-50">
+    <Navbar class="fixed top-0 left-0 w-full z-50" />
+    <div class="pt-20 px-6">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Additional styling if needed */
+</style>
